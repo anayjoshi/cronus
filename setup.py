1ae7772e8173600a701be4fa93bb0546f0eb3b03
Anay Joshi <anaykumar.joshi@gmail.com>
2014-05-20 01:03:38 +0530

    minor change to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
+
 setup(name='cronus',
     version='0.2',
     description='The Greek God of Time',
+    long_description='The documentation of cronus can be found at http://cronus.readthedocs.org',
     url='http://github.com/anayjoshi/cronus',
     author='Anaykumar Joshi',
     author_email='anaykumar.joshi@gmail.com',
