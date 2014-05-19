87d4b1d462133b57eb00bed35da1267afda0ca9f
Anay Joshi <anaykumar.joshi@gmail.com>
2014-05-19 23:12:12 +0530

    added documentation

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('/Users/anaykumarjoshi/devcodes/github/cronus/cronus'))
 
 # -- General configuration ------------------------------------------------
 
@@ -28,7 +28,6 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -260,7 +259,7 @@ texinfo_documents = [
 import alabaster
 
 html_theme_path = [alabaster.get_path()]
-extensions = ['alabaster']
+extensions = ['alabaster','sphinx.ext.autodoc',]
 html_theme = 'alabaster'
 html_sidebars = {
    '**': [
@@ -268,7 +267,6 @@ html_sidebars = {
    ]
 }
 html_theme_options = {
-    'logo': 'logo.png',
     'github_user': 'anayjoshi',
     'github_repo': 'cronus',
 }
